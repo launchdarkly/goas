ac14f7d2ae78a14df788e106feaa90257b4af11d
Dan O'Brien <dobrien@launchdarkly.com>
2021-07-13 16:43:58 -0400

    add test coverage for utils

---
+++ util_test.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_genSchemaObjectID(t *testing.T) {
+	t.Run("check none", func(t *testing.T) {
+		emptyMap := make(map[string]*string)
+		result := genSchemaObjectID("", "sample", emptyMap)
+
+		require.Equal(t, "sample", string(result))
+	})
+	t.Run("check single", func(t *testing.T) {
+		emptyMap := make(map[string]*string)
+		result := genSchemaObjectID("sample", "sample", emptyMap)
+
+		require.Equal(t, "sample.sample", string(result))
+	})
+	t.Run("check multiple", func(t *testing.T) {
+		emptyMap := make(map[string]*string)
+		result := genSchemaObjectID("test.sample", "sample", emptyMap)
+
+		require.Equal(t, "test.sample.sample", string(result))
+	})
+}
+
+func Test_checkAndMutateTypeName(t *testing.T) {
+	t.Run("check no change", func(t *testing.T) {
+		emptyMap := make(map[string]*string)
+		result := checkAndMutateTypeName("mypackage.test", emptyMap)
+
+		require.Equal(t, "mypackage.test", string(result))
+	})
+
+	t.Run("check empty rename", func(t *testing.T) {
+		emptyMap := make(map[string]*string)
+		emptyString := ""
+		emptyMap["mypackage"] = &emptyString
+		result := checkAndMutateTypeName("mypackage.test", emptyMap)
+
+		require.Equal(t, "test", string(result))
+	})
+
+	t.Run("check alias rename", func(t *testing.T) {
+		emptyMap := make(map[string]*string)
+		emptyString := "newpackage"
+		emptyMap["mypackage"] = &emptyString
+		result := checkAndMutateTypeName("mypackage.test", emptyMap)
+
+		require.Equal(t, "newpackage.test", string(result))
+	})
+}
+
+func Test_checkAndMutatePackageName(t *testing.T) {
+	pkgName := "example/go/mypackage"
+	t.Run("check no change", func(t *testing.T) {
+		emptyMap := make(map[string]*string)
+		result := checkAndMutatePackageName(pkgName, emptyMap)
+
+		require.Equal(t, "example/go/mypackage", string(result))
+	})
+
+	t.Run("check empty rename", func(t *testing.T) {
+		emptyMap := make(map[string]*string)
+		emptyString := ""
+		emptyMap["mypackage"] = &emptyString
+		result := checkAndMutatePackageName(pkgName, emptyMap)
+
+		require.Equal(t, "", string(result))
+	})
+
+	t.Run("check rename alias package", func(t *testing.T) {
+		emptyMap := make(map[string]*string)
+		emptyString := "newpackage"
+		emptyMap["mypackage"] = &emptyString
+		result := checkAndMutatePackageName(pkgName, emptyMap)
+
+		require.Equal(t, "newpackage", string(result))
+	})
+}
